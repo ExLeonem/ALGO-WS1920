96017202b8077a6d6fa35e459e4103ce15c3d5a7
exleonem <maksim.sandybekov@live.de>
2019-11-06 19:22:14 +0100

    quicksort working

---
+++ src/test/java/QuickSortTest.java
@@ -0,0 +1,40 @@
+import divide_conquer.QuickSort;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class QuickSortTest {
+
+    @Test
+    void singleElement() {
+        int[] elements = {1};
+        int[] result = {1};
+        assertArrayEquals(QuickSort.sort(elements), result);
+    }
+
+    @Test
+    void twoElementUnsorted() {
+        int[] elements = {2,1};
+        int[] results = {1,2};
+
+        assertArrayEquals(QuickSort.sort(elements), results);
+    }
+
+    @Test
+    void threeElementsReverseOrder() {
+        int[] elements = {3,2,1};
+        int[] results = {1,2,3};
+
+        assertArrayEquals(QuickSort.sort(elements), results);
+    }
+
+    @Test
+    void fourElementsRandomized() {
+        int[] elements = new int[]{6,3,21,4,4,5,2,1};
+        int[] result = new int[]{1,2,3,4,4,5,6,21};
+
+        assertArrayEquals(QuickSort.sort(elements), result);
+    }
+
+}
