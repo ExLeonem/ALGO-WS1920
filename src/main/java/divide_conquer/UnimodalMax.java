c1dfa3160232d3f44ede4d93bdd32bbc3dbe2050
exleonem <maksim.sandybekov@live.de>
2019-12-04 01:10:17 +0100

    search in unimodal list

--- src/main/java/divide_conquer/UnimodalMax.java
+++ src/main/java/divide_conquer/UnimodalMax.java
@@ -30,7 +30,7 @@ public class UnimodalMax {
             throw new IllegalArgumentException("Can't search for maximum value in an empty array.");
         }
 
-        return this.recurse(unimodalArray, 0, unimodalArray.length);
+        return this.recurse(unimodalArray, 0, unimodalArray.length-1);
     }
 
 
@@ -49,10 +49,22 @@ public class UnimodalMax {
         }
 
         int center = (left + right) / 2;
-        if (unimodalArray[center] > unimodalArray[left]) {
+        if (unimodalArray[center] > unimodalArray[right] && unimodalArray[center] > unimodalArray[left]) {
             return this.recurse(unimodalArray, center, right);
         }
+
+        // outter right element is biggest element
+        if (unimodalArray[center] < unimodalArray[right]) {
+            return this.recurse(unimodalArray, center+1, right);
+        }
+
         return this.recurse(unimodalArray, left, center);
+
+//        if (unimodalArray[center] > unimodalArray[left]) {
+//            // right is decreasing.
+//            return this.recurse(unimodalArray, center, right);
+//        }
+//        return this.recurse(unimodalArray, left, center);
     }
 
 
