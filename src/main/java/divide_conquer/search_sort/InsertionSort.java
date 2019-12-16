b0c0cb595267d3fcd12e0c84f91ed49739579321
exleonem <maksim.sandybekov@live.de>
2019-12-16 23:17:02 +0100

    implemented insertion sort

--- src/main/java/divide_conquer/search_sort/InsertionSort.java
+++ src/main/java/divide_conquer/search_sort/InsertionSort.java
@@ -1,6 +1,21 @@
 package divide_conquer.search_sort;
 
+import java.util.Arrays;
+
 public class InsertionSort {
+
+
+    private Order order;
+
+
+    public InsertionSort(Order order) {
+        this.order = order;
+    }
+
+    public InsertionSort() {
+        this.order = Order.ASC;
+    }
+
 
 
     /**
@@ -11,13 +26,16 @@ public class InsertionSort {
      */
     public int[] sort(int[] items) {
 
+        // Edge case, more elements needed to perform sorting
         if (items.length <= 1) {
             return items;
         }
 
+        System.out.println("Array: " + Arrays.toString(items));
         this.sort(items, 0, items.length);
         return items;
     }
+
 
     /**
      * Sort a sub-array in-place with insertion-sort.
@@ -28,28 +46,76 @@ public class InsertionSort {
      */
     public void sort(int[] items, int left, int right) {
 
-
-        for (int i = left; i < items.length; i++) {
+        for (int i = left; i < right; i++) {
 
             // Sort items till this position
-            for (int j = left; j >= 0; j--) {
+            int lastIndx = i;
+            for (int j = i; j >= 0; j--) {
 
-                if (items[i] < items[j]) {
-
+                // Identify index of item
+//                System.out.println("Shift Item left + [" + items[i] + "] + [" + items[j] + "]");
+                if (this.getOrder().inOrder(items[i], items[j])) {
+                    lastIndx = j;
                 }
-
             }
+
+            // Shift all items after indx to the right
+            this.shiftItems(items, lastIndx, i);
+//            System.out.println("Shifted: " + Arrays.toString(items));
+//            System.out.println("------------------------");
         }
     }
 
 
     /**
      * Shift items from left border to right border
-     * @param items
-     * @param left
-     * @param right
+     *
+     * @param items - items to shift to the right
+     * @param rightIndx - Index where to start shifting
+     * @param valueIndx - right outter index to which to shift items
      */
-    private void shiftItems(int[] items, int left, int right) {
+    private void shiftItems(int[] items, int rightIndx, int valueIndx) {
 
+        // Last element needs to be put to place
+        int temp = items[valueIndx];
+
+//        System.out.println("Right: " + rightIndx);
+//        System.out.println("Value-Indx: " + valueIndx);
+        // Shift items
+        for (int i = valueIndx; i > rightIndx; i--) {
+//            System.out.println("Swap: [" + i + "] mit [" + (i-1) + "]");
+            swap(items, i, i-1);
+        }
+
+        items[rightIndx] = temp;
     }
+
+
+    /**
+     * Swap the positions of two items.
+     *
+     * @param items - the items to switch
+     * @param from - swap from this index to the other
+     * @param to - swap to this index
+     */
+    private void swap(int[] items, int from, int to) {
+
+        int temp = items[from];
+        items[from] = items[to];
+        items[to] = temp;
+    }
+
+
+    // -------------------------
+    // Setter/-Getter
+    // -------------------------
+
+    private Order getOrder() {
+        return this.order;
+    }
+
+    private void setOrder(Order order) {
+        this.order = order;
+    }
+
 }
--- src/test/java/divider_conquer/search_sort/InsertionSortTest.java
+++ src/test/java/divider_conquer/search_sort/InsertionSortTest.java
@@ -3,6 +3,11 @@ package divider_conquer.search_sort;
 import divide_conquer.search_sort.InsertionSort;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Random;
+
 import static org.junit.Assert.assertArrayEquals;
 
 public class InsertionSortTest {
@@ -31,10 +36,63 @@ public class InsertionSortTest {
 
 
     @Test
+    void testTwoElements() {
+        int[] items = new int[]{5, 3};
+        int[] actual = insertion.sort(items);
+        int[] expected = new int[]{3, 5};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
     void reverseElements() {
         int[] items = new int[]{5, 3, 2, 1};
         int[] actual = insertion.sort(items);
         int[] expected = new int[]{1, 2, 3, 5};
+
+        assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    void random() {
+        PriorityQueue<Integer> sortedValues = new PriorityQueue<Integer>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (o1.intValue() > o2.intValue()) {
+                    return 1;
+                } else if (o1.intValue() == o2.intValue()) {
+                    return 0;
+                }
+                return -1;
+            }
+        });
+
+        // Test parameter
+        final int ARRAY_LENGTH = 200;
+        final int MAX_VALUE = 200;
+        final int MIN_VALUE = 200;
+
+        // Generate random values copy into priority queue and keep unsorted array
+        Random rand = new Random();
+        int[] items = new int[rand.nextInt(ARRAY_LENGTH) + MIN_VALUE];
+        for (int i = 0; i < items.length; i++) {
+            int nextElement = rand.nextInt(MAX_VALUE) + MIN_VALUE;
+            items[i] = nextElement;
+            sortedValues.add(nextElement);
+        }
+
+        // Copy sorted values into array
+        int[] actual = insertion.sort(items);
+        int[] expected = new int[items.length];
+        int indx = 0;
+        while (!sortedValues.isEmpty()) {
+            expected[indx] = sortedValues.poll();
+            indx++;
+        }
+
+//        System.out.println("Sorted-by-method: " + Arrays.toString(actual));
+//        System.out.println("Expected-Sorting: " + Arrays.toString(expected));
 
         assertArrayEquals(expected, actual);
     }
